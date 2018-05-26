d0c0094828b3cdf08e068710be830b1e3beeafd8
rob <rob@unx.pl>
2018-05-26 21:46:56 +0200

    add prometheus notifications

--- duplicity-backup.sh
+++ duplicity-backup.sh
@@ -3,6 +3,8 @@
 zabbix_server=$ZABBIX_SERVER
 zabbix_key="backup.duplicity"
 zabbix_key_duration="backup.duplicity.duration"
+prometheus_pushgateway_url=$PROMETHEUS_PUSHGATEWAY_URL
+prometheus_job=$PROMETHEUS_JOB
 hostname=`hostname | cut -d\. -f1`
 archive_dir="/var/lib/duplicity"
 archive_name=${ARCHIVE_NAME:-$hostname}
@@ -38,7 +40,7 @@ function notify_zabbix {
 	local key="$1"
 	local value="$2"
 	if [ -n "$zabbix_server" ]; then
-		write_log "INFO: notify zabbix: key $key: "
+		write_log "INFO: notify zabbix: $key: $value"
 		zabbix_sender -z $zabbix_server -s $hostname -k $key -o $value 2>&1
 	fi
 }
@@ -48,6 +50,33 @@ function send_annotation {
 	if [ -n "$influxdb_url" ]; then
 		write_log "INFO: sending annotation: $state"
 		curl -s -XPOST "${influxdb_url}/write?db=$influxdb_dbname" --data-binary 'annotations,host='$hostname' title="backup duplicity '$state'"'
+	fi
+}
+
+function notify_prometheus {
+	local success="$1"
+	local duration="$2"
+	if [ -n "$prometheus_pushgateway_url" ] && [ -n "$prometheus_job" ]; then
+		write_log "INFO: notify prometheus: success: $success; duration: $duration"
+		if [ "$success" -eq 1 ]; then
+cat <<EOF | curl -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
+# HELP batchjob_duration_seconds Duration of batch job
+# TYPE batchjob_duration_seconds gauge
+batchjob_duration_seconds $duration
+# HELP batchjob_last_success Unixtime batch job last succeeded
+# TYPE batchjob_last_success gauge
+batchjob_last_success $(date +%s.%7N)
+# HELP batchjob_success Success of batch job
+# TYPE batchjob_success gauge
+batchjob_success 1
+EOF
+		else
+cat <<EOF | curl -XPOST --data-binary @- ${prometheus_pushgateway_url}/metrics/job/${prometheus_job}/instance/$hostname
+# HELP batchjob_success Success of batch job
+# TYPE batchjob_success gauge
+batchjob_success 0
+EOF
+		fi
 	fi
 }
 
@@ -71,12 +100,19 @@ if [ "`echo "$output" | grep -P '^Errors 0'`" ]; then
 	write_log "INFO: duplicity remove old backups"
 	duplicity remove-older-than $remove_older_than $opts --force $url 2>&1
 
-	notify_zabbix $zabbix_key 1
 	duration=$((`date +'%s'` - $start_timastamp))
+
+	notify_zabbix $zabbix_key 1
 	notify_zabbix $zabbix_key_duration $duration
+
+	notify_prometheus 1 $duration
+
 	write_log "INFO: duplicity stop, backup duration: $duration s"
 else
 	notify_zabbix $zabbix_key 0
+
+	notify_prometheus 0
+
 	write_log "ERROR: duplicity finished with errors"
 fi
 
